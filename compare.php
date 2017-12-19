e1817986ab7ac490a7227158dcba3e75e8ea9b4f
zeeeya <zci.alvarez@gmail.com>
2017-12-19 19:34:05 +0800

    yes

--- compare.php
+++ compare.php
@@ -1,3 +1,18 @@
+<?php 
+  include('database.php');
+
+  $pdo = Database::connect();
+  if(!empty($_GET['id'])){
+    $prod_code = $_REQUEST['id'];
+
+    $prod = $pdo->prepare("SELECT * FROM product WHERE prod_code = ?");
+    $prod->execute(array($prod_code));
+    $prod = $prod->fetch(PDO::FETCH_ASSOC);
+
+  }else{
+    header('location:index.php');
+  }
+?>
 <!--A Design by W3layouts 
 Author: W3layout
 Author URL: http://w3layouts.com
@@ -5,111 +20,36 @@ License: Creative Commons Attribution 3.0 Unported
 License URL: http://creativecommons.org/licenses/by/3.0/
 -->
 <!DOCTYPE html>
+<style type="text/css">
+  .img-responsive{
+  height: 500px;
+  width: 500px;
+  }
+</style>
 <html>
 <?php include('head.php'); ?>
-<style>
-* {
-    box-sizing: border-box;
-}
-
-body {
-    margin: 0;
-}
-
-/* Create three equal columns that floats next to each other */
-.column {
-    float: left;
-    width: 33.33%;
-    padding: 10px;
-    height: 300px; /* Should be removed. Only for demonstration */
-}
-
-/* Clear floats after the columns */
-.row:after {
-    content: "";
-    display: table;
-    clear: both;
-}
-</style>
 <body>
-<!--header-->
 <?php include('header.php'); ?>
   <!-- grow -->
-  <div class="grow">
+  <div class="alert alert-success">
     <div class="container">
-      <h2 style="font-family: verdana">Place Order</h2>
+      <h2 style="font-family: verdana;"><?php echo $prod['prod_name'] ?></h2>
     </div>
   </div>
   <!-- grow -->
-<!--content-->
-      
-  
-  <h3 style="font-family: verdana; background-color: #ebebc6; text-align: center; ">Product Summary</h3>
-  <div class="container-fluid" style="width: 12.5in;">
-        <div class="col-lg-12">
-          <div class="panel panel-default">
-            <div class="panel-heading">
-              <span></span>
-              <br>
-            </div>
-            <div class="panel-body">
-              <form action="setaddress.php" enctype="multipart/form-data" method="post">
-                <div class="row">
-                    <div class="column simpleCart_shelfItem">
-            <div class="product-at ">
-              <a href="single.php"><img class="img-responsive" src="images/pi3.jpg" alt="">
-              <div class="pro-grid">
-                    <span class="buy-in">Buy Now</span>
-              </div>
-            </a>  
-            </div>
-            <p class="tun">CLARISSA</p>
-            <div class="ca-rt">
-              <a href="#" class="item_add"><p class="number item_price"><i> </i>$500.00</p></a>           
-            </div>
-          </div>
-                  </div>
-                  <div class="column">
-                    <span>2</span>
-                  </div>
-                  <div class="column">
-                    <span>3</span>
-                  </div>
-                </div>
-              </div>
-        </div>
-        <br>
-        <br>
-         <button type="submit" class="w3-button pull-right" onclick="plusDivs(1)" style=" font-family: verdana; background-color: #8de78b; color: white; font-weight: bold;"> Address &#10095;</button>
-         </form>
-      </div>
+    <div class="product">
+      <div class="container">
+        <div class="product-price1">
+        <div class="top-sing">
+        <div class="col-md-7 single-top"> 
+            <div class="flexslider">
+              <div> <img <?php echo "src=prod_img/" . $prod['prod_image'] ?> data-imagezoom="true" class="img-responsive"> </div>
     </div>
 
-    <br>
-    <br>
-<!--//content-->
-<!--
-<script>
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
-}
-</script> -->
-<!--footer-->
-<?php include('footer.php'); ?>
+  </div>
+</div>
+</div>
+</div>
+</div>
 </body>
 </html>
-      
