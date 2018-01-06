dd98a74dd0e89c7b98360ac5e5b00d19b48dbbef
zeeeya <zci.alvarez@gmail.com>
2018-01-06 16:16:35 +0800

    aye

--- php/check.php
+++ php/check.php
@@ -2,30 +2,31 @@
 	session_start();
 	require '../database.php';
 	
-	$name = $_POST['name'];
-	$acc_company = $_POST['acc_company'];
+	
 	$shippingaddress = $_POST['shippingaddress'];
 	$country = $_POST['country'];
 	$state = $_POST['state'];
 	$city = $_POST['city'];
-	$acc_email = $_POST['acc_email'];
-	$acc_contact = $_POST['acc_contact'];
 	$zip_code = $_POST['zip_code'];
 	
 
 	$pdo = Database::connect();
-
 	$acc_email =  $_SESSION['login_username'];
 	$user_id = $pdo->prepare("SELECT acc_id FROM account WHERE acc_email = '$acc_email'");
 	$user_id->execute();
 	$user_id = $user_id->fetch(PDO::FETCH_ASSOC);
 
+	$pdo = Database::connect();
+	$acc_email =  $_SESSION['login_username'];
+	$user_id = $pdo->prepare("SELECT acc_id FROM account WHERE acc_email = '$acc_email'");
+	$user_id->execute();
+	$user_id = $user_id->fetch(PDO::FETCH_ASSOC);
 
 	$pdo = Database::connect();
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$sql = "INSERT INTO orders (name, acc_company, shippingaddress, country, state, city, acc_email, acc_contact, zip_code) values(?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		$sql = "INSERT INTO orders (shippingaddress, country, state, city, zip_code) values(?, ?, ?, ?, ?)";
 		$q = $pdo->prepare($sql);
-        $q->execute(array($name, $acc_company, $shippingaddress, $country, $state, $city, $acc_email, $acc_contact, $zip_code));
+        $q->execute(array($shippingaddress, $country, $state, $city, $zip_code));
         Database::disconnect();
 
 
